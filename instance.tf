0bd34ba0d52f5486bfc80274e4e1432d7413c50b
Aaron Trout <aaron@trouter.me>
2021-08-01 08:27:20 +0100

    Mark the startup script as sensitive
    It contains the tailscale auth key

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "tailscale" {
   name                    = "tailscale"
   machine_type            = var.instance_type
-  metadata_startup_script = data.template_file.startup_script.rendered
+  metadata_startup_script = sensitive(data.template_file.startup_script.rendered)
 
   boot_disk {
     initialize_params {
