c504e4cb0a9c04533e792220ec59ce5459c7310f
Aaron Trout <aaron@trouter.me>
2021-08-01 07:49:19 +0100

    First cut of instance creation

---
+++ instance.tf
@@ -0,0 +1,31 @@
+resource "google_compute_instance" "tailscale" {
+  name = "tailscale"
+  machine_type = var.instance_type
+  metadata_startup_script = data.template_file.startup_script.rendered
+
+  boot_disk {
+    initialize_params {
+      type = "pd-standard"
+      size = 40
+      image = "ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.tailscale.self_link
+  }
+
+  shielded_instance_config {
+    enable_secure_boot = true
+  }
+
+}
+
+data "template_file" "startup_script" {
+  template = "${file("${path.module}/startup_script.sh.tpl")}"
+
+  vars = {
+    authkey = var.tailscale_auth_key
+    hostname = var.hostname
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "vpc_subnet_cidr" {
+  description = "CIDR to use for the GCP subnet. Default: 10.224.0.0/26"
+  type = string
+  default = "10.224.0.0/24"
+}
+
+variable "instance_type" {
+  description = "GCP instance type to use. Default: f1-micro"
+  type = string
+  default = "f1-micro"
+}
+
+variable "tailscale_auth_key" {
+  description = "Tailscale authentication key for the node (required)"
+  type = string
+}
+
+variable "hostname" {
+  description = "Hostname to use when connecting to tailscale. Default: tailscale-relay"
+  type = string
+  default = "tailscale-relay"
+}
